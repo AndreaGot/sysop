7a3bc97803a4f67c70ae28e6f2c67d66c56612ec
Andrea Gottardi <andreagot16@gmail.com>
2013-05-08 15:32:24 +0200

    mkbkp now works, still need extraction and we are done

--- mkbkp/mkbkp.c
+++ mkbkp/mkbkp.c
@@ -33,6 +33,8 @@ void show_file (const char *filename, FILE *out);
 void creabkp(int numpar, char * param[], int ind);
 void stampa();
 int list(const char *name, const struct stat *status, int type);
+int listC(const char *name, const struct stat *status, int type);
+int listD(const char *name, const struct stat *status, int type);
 
 
 
@@ -41,8 +43,8 @@ int main(int argc, char * argv[])
 {
 	int i;
 	int opt = 0;
-	bool f = false;
-	bool c = false;
+	bool f = false;													// Setto tutte le variabili a false
+	bool c = false;													// mi serviranno per capire che funzione chiamare
 	bool x = false;
 	bool t = false;
 while ( (i = getopt(argc, argv, "fcxt")) != -1)
@@ -76,8 +78,8 @@ while ( (i = getopt(argc, argv, "fcxt")) != -1)
 		}
 	}
 	
-	globalargv = argv;
-	globalargc = argc;
+	globalargv = argv;														//salvo la lista dei parametri
+	globalargc = argc;														//e il numero di essi, mi serviranno poi
 	
 	if(opt>2)
 	{
@@ -119,36 +121,31 @@ while ( (i = getopt(argc, argv, "fcxt")) != -1)
 
 
 
-
-
-
-
-
 char * scrivi(char * a, char * b)
 {
 	
-	char *targetdir = malloc(2048);
-	strcpy(targetdir,a);
-	strcat(targetdir,"/");
-	strcat(targetdir,b);
+	char *targetdir = malloc(2048);											//alloco 2048 byte di memoria
+	strcpy(targetdir,a);													//copio a nella stringa creata poco fa
+	strcat(targetdir,"/");													// aggiungo il carattere / (slash)
+	strcat(targetdir,b);													//copio anche b nella stringa, concatenandolo			
 	
 	printf("%s \n", targetdir);	
-	return targetdir;
+	return targetdir;														// e il nuovo percorso è fatto
 }
 
 
 
 
-void show_file (const char *filename, FILE *out) {
+void show_file (const char *filename, FILE *out) {							//scrive il contenuto di un file in un altro, carattere per carattere (così si mantengono gli whitespace)
     int c;
     FILE *file;
     //fprintf(out, "%s(%s) BEGIN\n", __func__, filename);
-    file = fopen(filename, "r");
-    if (file) {
-        while ((c = fgetc(file)) != EOF) {
-            fputc(c, out);
-        }
-        fclose(file);
+    file = fopen(filename, "r");											//apro il file
+    if (file) {																//se è aperto
+        while ((c = fgetc(file)) != EOF) {									//e il carattere preso non eccede la fine del file
+            fputc(c, out);													//scrivo lo stesso carattere in out (file in uscita)
+        }																	//
+        fclose(file);														//chiudo il file
     } else {
         fprintf(out, "%s: failed to open file '%s' (%d)\n", __func__, filename, errno);
     }
@@ -165,11 +162,83 @@ void creabkp(int numpar, char * param[], int ind)
 	archivio = scrivi(getcwd(NULL, 0), nome);
     arch = fopen(archivio, "w+");
 	
-	int i=ind+3;
 	
+	//-----------------------------------------------------------------------------------------------------------------------------------
+	// dai file passati in input viene ricavato un archivio con la seguente struttura:
+	//
+	//		%LIST% Lista dei file nell'archivio %LIST%
+	//		%DIRS% Lista delle cartelle nell'archivio %DIRS%
+	//		%CONTENT% Contenuto file 1   %CONTENT%
+	//		%CONTENT% Contenuto file ... %CONTENT%
+	//		%CONTENT% Contenuto file  N  %CONTENT%
+	//
+	//------------------------------------------------------------------------------------------------------------------------------------
+	
+	
+	//-----------------------------------------  LISTA DEI FILE CONTENUTI NELL'ARCHIVIO --------------------------------------------------
+	
+	int i=ind+3;
 	
 	fprintf(arch, "%s", LIST);
 	fprintf(arch, "%s", " ");
+	
+	while(i<=numpar)
+	{
+		
+		
+		if ((opendir(param[i-1])==NULL))									// Se l'operazione di apertura cartella non va a buon fine
+		{																	// Allora è un file
+			copy = strdup(param[i-1]);										// copio in una stringa il parametro di argv
+			filename = basename(param[i-1]);								// estraggo il nome dal percorso
+			puts(filename);													//
+			fprintf(arch, "/%s", filename);									// e lo stampo nel file
+			
+		}
+		else 
+		{
+			lung = strlen(param[i-1]);										// se è una cartella
+			puts("this... is.. A FOLDEEEEEEEEEER");							//
+			ftw(param[i-1], list, 1);										//scorrila tutta	
+		}
+		fprintf(arch, "%s", " ");
+		i++;
+	}
+	
+	fprintf(arch, "%s", " ");
+	fprintf(arch, "%s", LIST);
+	fprintf(arch, "%s", "\n");
+	
+	
+	//-------------------------------------------------------- FINE LISTA ------------------------------------------------------------------
+	// -----------------------------------------------------INIZIO DIRECTORY ----------------------------------------------------------------
+	i=ind+3;
+
+	fprintf(arch, "%s", DIRS);
+	fprintf(arch, "%s", " ");
+	while(i<=numpar)
+	{
+	
+		if ((opendir(param[i-1])==NULL))
+		{}else{
+			lung = strlen(param[i-1]);
+			ftw(param[i-1], listD, 1);
+		}
+		fprintf(arch, "%s", " ");
+		i++;
+
+	
+	}
+	
+	fprintf(arch, "%s", " ");
+	fprintf(arch, "%s", DIRS);
+	fprintf(arch, "%s", "\n");
+	
+	//---------------------------------------------------------FINE DIRECTORY-------------------------------------------------------------
+	//----------------------------------------------------------INIZIO CONTENT------------------------------------------------------------
+	
+	
+	i=ind+3;
+	
 	
 	while(i<=numpar)
 	{
@@ -177,46 +246,20 @@ void creabkp(int numpar, char * param[], int ind)
 		
 		if ((opendir(param[i-1])==NULL))
 		{
-			copy = strdup(param[i-1]);
-			filename = basename(param[i-1]);
-			puts(filename);	
-			fprintf(arch, "%s", filename);
+			fprintf(arch, "%s", CONTENT);
 			fprintf(arch, "%s", " ");
+			copy = strdup(param[i-1]);
+			show_file(copy, arch);
+			fprintf(arch, "%s", " ");
+			fprintf(arch, "%s \n", CONTENT);
+			
 		}
 		else 
 		{
 			lung = strlen(param[i-1])+1;
 			puts("this... is.. A FOLDEEEEEEEEEER");
-			ftw(param[i-1], list, 1);
+			ftw(param[i-1], listC, 1);
 		}
-
-		i++;
-	}
-	
-	fprintf(arch, "%s", " ");
-	fprintf(arch, "%s", LIST);
-	fprintf(arch, "%s", "\n");
-	
-	
-	fprintf(arch, "%s", DIRS);
-	fprintf(arch, "%s", " ");
-	
-	//INSERIRE UN WHILE CHE INSERISCE LE EVENTUALI CARTELLE FORMANTI IL PERCORSO
-	
-	fprintf(arch, "%s", " ");
-	fprintf(arch, "%s", DIRS);
-	fprintf(arch, "%s", "\n");
-	
-	i=ind+3;
-	
-	
-	while(i<=numpar)
-	{
-		fprintf(arch, "%s", CONTENT);
-		fprintf(arch, "%s", " ");
-		show_file(param[i-1], arch);
-		fprintf(arch, "%s", " ");
-		fprintf(arch, "%s", CONTENT);
 		
 		fprintf(arch, "%s", "\n");
 		
@@ -224,6 +267,8 @@ void creabkp(int numpar, char * param[], int ind)
 		
 		
 	}
+	
+	//-------------------------------------------------------FINE CONTENT-----------------------------------------------------------------------
 	fclose(arch);
 }
 
@@ -234,12 +279,15 @@ void read_words (FILE *f) {
 		if(strcmp(x, "%LIST%")==0 && listTrovata==false)
 		{
 			listTrovata= true;
-			printf("trovato il primo \n");
+			puts("trovato il primo \n");
 			continue;
 		}
 		else if (strcmp(x, "%LIST%")==0 && listTrovata)
 		{
+			long position;
+			position = ftell(f);
 			printf("trovato il secondo \n");
+			printf("LIST trovato alla posizione %ld", position);
 			fseek(f, 0, SEEK_END);
 			continue;
 		}
@@ -252,24 +300,79 @@ void read_words (FILE *f) {
 	
 }
 
+
+
+
+
+
+//-------------------------------------------------------INIZIO LIST PER %LIST%-----------------------------------------------------------------------
+
 int list(const char *name, const struct stat *status, int type) {
 	if(type == FTW_NS)
 		return 0;
 	
 	if(type == FTW_D && strcmp(".", name) != 0 && strcmp("..", name) != 0)
-		printf("0%3o\t%s/\n", status->st_mode&0777, name);
-	
-	if(type == FTW_F)
+	{
+		printf("0%3o\t%s\n", status->st_mode&0777, name);
+	}
+	else if(type == FTW_F)
+	{
 		printf("0%3o\t%s\n", status->st_mode&0777, name);
 		fprintf(arch, "%s ", name + lung);
 		printf( "%s ", name + lung);
-		
-	
-
+	}	
 	
 	return 0;
 }
 
+//-------------------------------------------------------FINE LIST-----------------------------------------------------------------------
+//-------------------------------------------------------INIZIO LISTC PER %CONTENT%-----------------------------------------------------------------------
+
+int listC(const char *name, const struct stat *status, int type) {
+	if(type == FTW_NS)
+		return 0;
+	
+	if(type == FTW_D && strcmp(".", name) != 0)
+		printf("0%3o\t%s/\n", status->st_mode&0777, name);
+	
+	if(type == FTW_F)
+	{
+		fprintf(arch, "%s", CONTENT);
+		fprintf(arch, "%s", " ");
+		printf("0%3o\t%s\n", status->st_mode&0777, name);
+		show_file(name, arch);
+		fprintf(arch, "%s", " ");
+		fprintf(arch, "%s", CONTENT);
+		fprintf(arch, "\n", CONTENT);
+		
+	}
+	
+	
+	
+	return 0;
+}
+
+//-------------------------------------------------------FINE LISTC-----------------------------------------------------------------------
+//-------------------------------------------------------INIZIO LISTD PER %DIRS%-----------------------------------------------------------------------
+
+int listD(const char *name, const struct stat *status, int type) {
+	if(type == FTW_NS)
+		return 0;
+	
+	if(type == FTW_D && strcmp(".", name) != 0 && strcmp("..", name) != 0)
+	{
+		puts("entrato");
+		printf("0%3o\t%s\n", status->st_mode&0777, name);
+		fprintf(arch, "%s ", name + lung);
+		puts("fatto");
+		printf( "%s ", name + lung);
+	}
+	puts("uscito");
+	
+	return 0;
+}
+
+//-------------------------------------------------------FINE LISTD-----------------------------------------------------------------------
 
 void stampa()
 {
@@ -279,6 +382,8 @@ void stampa()
     ciao = fopen(daListare, "r");
 	read_words(ciao);
 }
+
+
 
 void usage()
 {
