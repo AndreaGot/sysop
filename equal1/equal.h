90e79cfe1fdb615bb10d34c18e3081bbd2505194
giTorto <root@giulian-S400CA.(none)>
2013-05-30 17:04:07 +0200

    pulizia cartelle

--- equal1/equal.h
+++
@@ -1,7 +0,0 @@
-bool isDIR(char * folder);
-void compareArray(char** first,char** second, int fLun,int sLun,char* fHead, char* sHead);
-void savedir(char *folder1, char*** percorsi,int* i,int* n,char* radice);
-char** scorripercorso(char* folder,int* n);
-int comp(const void *x, const void *y);
-void verificaLast(char** x, char* y, char* add);
-bool compareFile(char* primo, char* secondo, bool* primavolta);
